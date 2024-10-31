96fd8de9d1ece4594f9764d8016389be94cdeb5c
e-nk <enocklagat747@gmail.com>
2024-10-31 17:23:20 +0300

    user-post relationship updated

--- lib/forum_web/controllers/user_json.ex
+++ lib/forum_web/controllers/user_json.ex
@@ -1,9 +1,17 @@
 defmodule ForumWeb.UserJSON do
   alias Forum.Accounts.User
+  alias Forum.Posts.Post
+
+  @doc """
+  Renders a list of posts.
+  """
+  def index(%{posts: posts}) do
+
 
   @doc """
   Renders a list of users.
   """
+  
   def index(%{users: users}) do
     %{data: for(user <- users, do: data(user))}
   end
@@ -19,7 +27,16 @@ defmodule ForumWeb.UserJSON do
     %{
       id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      posts: for(post <- user.posts, do: post_data(post))
+    }
+  end
+
+  defp post_data(%Post{} = post) do
+    %{
+      id: post.id,
+      title: post.title,
+      body: post.body
     }
   end
 end
