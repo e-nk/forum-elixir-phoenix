32cc669eea41b5eb95fb4857572e363460d6a9aa
e-nk <enocklagat747@gmail.com>
2024-10-31 16:21:43 +0300

    api setup

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,12 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "John", email: "john@mail.com"},
+      %{id: 2, name: "Jane", email: "jane@mail.com"},
+      %{id: 3, name: "Bob", email: "bob@mail.com"}
+    ]
+    json(conn, %{users: users})
+  end
 end
